73689a4bb6236838ea17cac9b78500d1e76891ca
FernandoBDansi <fernandobuenodansi@gmail.com>
2016-04-19 21:17:02 -0300

    Iniciando em C

---
+++ Algoritmos em C/Altura-Atletas.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#define TAM 3
+
+void impAltMenorMaior(float alturaAtletas[TAM]){
+  float soma=0,media;
+  int i;
+    for(i=0;i<TAM;i++){
+      soma = soma+alturaAtletas[i];
+    }
+
+      media = soma/TAM;
+      printf("A media de Altura e %.2f\n",media);
+    for(i=0;i<TAM;i++){
+      if(alturaAtletas[i]<media){
+        printf("o Atleta %i tem altura menor que a media\n",i+1);
+      }
+    }
+}
+
+int main(){
+  float alturaAtletas[TAM];
+  int i;
+  system("clear");
+  //Lendo altura dos atletas
+  for(i=0;i<TAM;i++){
+  printf("Digite a altura do atletas %i\n",i+1);
+  scanf("%f",&alturaAtletas[i]);
+  }
+
+  impAltMenorMaior(alturaAtletas);
+  system("sleep 2");
+  return 0;
+}
---
+++ Algoritmos em C/Multiplo-de-7.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int multiploSete(int num){
+
+    if((num%7)==0){
+      return 1;
+    }else{
+      return 0;
+    }
+}
+
+int main(){
+  int i, cont=0;
+  int n;
+  //capturar o Valor de N
+  printf("Digite o numero:\n");
+  scanf("%i",&n);
+printf("########################### Multiplos de Sete###########################\n");
+  for(i=1;i<=n;i++){
+    //Verificar todos os numeros multiplos de 7
+    if(multiploSete(i)==1){
+        printf("Esse numero e multiplos de 7: %i\n",i);
+    }
+
+  }
+
+  printf("#######################################################################\n");
+  system("sleep 2");
+  return 0;
+}
