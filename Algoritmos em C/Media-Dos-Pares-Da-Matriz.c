17b24b45cbf12555120f4356d03575b6d91b9be3
Fernando Bueno Dansi <fernandobuenodansi@gmail.com>
2018-02-05 23:12:53 -0200

    Update Media-Dos-Pares-Da-Matriz.c

--- Algoritmos em C/Media-Dos-Pares-Da-Matriz.c
+++ Algoritmos em C/Media-Dos-Pares-Da-Matriz.c
@@ -8,7 +8,6 @@ int main(){
   int i=0,j=0;
   float media=0,soma=0,cont=0;
 
-
   for(i=0;i<LIN;i++){
     for(j=0;j<COL;j++){
 
@@ -24,13 +23,11 @@ int main(){
       if(matriz[i][j]%2==0){
         soma = soma + matriz[i][j];
         cont++;
-
       }
     }
   }
-
+  
   media = soma/cont;
-
   printf("A media eh: %.2f\n",media);
 
   return 0;
